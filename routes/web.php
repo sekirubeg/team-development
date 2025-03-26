dc40e07d1e203ee221b15362751a5bee4ace48b2
sekirubeg <niseyanagitetsuko@gmail.com>
2025-03-26 22:31:14 +0800

    ローカルでのweb.phpの変更

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\My_pageController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::get('/my_page', [App\Http\Controllers\My_pageController::class, 'index'])->name('my_page');
+
+Route::prefix('my_page')
+    ->middleware('auth')
+    ->group(function () {
+        Route::get('/', [My_pageController::class, 'index'])->name('my_page');
+        Route::get('/edit', [My_pageController::class, 'edit'])->name('my_page.edit');
+        Route::post('/update', [My_pageController::class, 'update'])->name('my_page.update');
+});
