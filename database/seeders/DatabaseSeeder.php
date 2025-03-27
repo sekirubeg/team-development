9ebf988ca2256955c3e6bc02d339418ba8bf2200
sekirubeg <niseyanagitetsuko@gmail.com>
2025-03-27 10:11:13 +0800

    ファクトリ

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +16,20 @@ class DatabaseSeeder extends Seeder
     {
         User::factory(10)->create();
 
+
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => "sekirubeg",
         ]);
+        Task::factory()->create([
+            'title' => "test",
+            'content' => "testtesttest",
+            'user_id' => 11,
+            'importance' => fake()->numberBetween(1, 5),
+            'limit' => fake()->date(),
+        ]);
+        Task::factory(20)->create();
     }
+
 }
