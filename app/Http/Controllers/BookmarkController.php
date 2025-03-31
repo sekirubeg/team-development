e89c31238ce4238f9054ae5b8adc2171de4aac1f
sekirubeg <niseyanagitetsuko@gmail.com>
2025-03-31 09:21:07 +0800

    3/31更新

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Task;
+use App\Notifications\LikeNotification;
 
 class BookmarkController extends Controller
 {
     //
     public function store(Request $request, $id)
     {
+        $task = Task::with('user')->findOrFail($id);
         Auth::user()->bookmark($id);
+        $task->user->notify(new LikeNotification($task));
+
         return back();
     }
     public function destroy(Request $request, $id)
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -12,7 +12,7 @@ class Task extends Model
 
     protected $guarded = [];
 
-    public function users()
+    public function user()
     {
         return $this->belongsTo(User::class);
     }
